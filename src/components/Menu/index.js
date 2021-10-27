f211160002ceadbbd67a7028b015c665cee4a524
Diego Fernando Alayon <dfar.41@gmail.com>
2021-10-27 16:31:40 -0500

    terminando menu responsive

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,13 +1,22 @@
+import menuImg from '../../images/list.png'
 import './menu.css'
-
 export default function Menu(){
   return (
     <nav>
-      <a className="option" href="/">Welcome</a>
-      <a className="option" href="/">Dropdown</a>
-      <a className="option" href="/">Left Sidebar</a>
-      <a className="option" href="/">Right Sidebar</a>
-      <a className="option" href="/">No Sidebar</a>
+      <input type="checkbox" id="check" className="check"/>
+      <label for="check" class="checkbtn"><img src={menuImg} alt="list-menu" width="40px"/></label>
+      <ul className="list-menu">
+        <li className="item-menu"><a className="option" href="/">Welcome</a></li>
+        <li className="item-menu"><a className="option" href="/">Dropdown</a></li>
+        <li className="item-menu"><a className="option" href="/">Left Sidebar</a></li>
+        <li className="item-menu"><a className="option" href="/">Right Sidebar</a></li>
+        <li className="item-menu"><a className="option" href="/">No Sidebar</a></li>
+      </ul>
+      
+      
+      
+      
+      
     </nav>
   )
 }
